d6b2fd2245785862ad79979d232641effb0f5321
callu9 <callu_9ine@naver.com>
2025-02-04 09:29:01 +0900

    refactor/feed: fix home page to use server side props to get feed list api

---
+++ src/pages/api/feeds/index.ts
@@ -0,0 +1,14 @@
+import { feeds } from "@/mocks";
+import { HTTP_STATUS_CODE, RESPONSE_MESSAGE, RESPONSE_NOT_METHOD_ALLOWED } from "@/mocks/util";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    return res.status(HTTP_STATUS_CODE.OK).json({
+      message: RESPONSE_MESSAGE.SUCCESS,
+      code: HTTP_STATUS_CODE.OK,
+      data: feeds,
+      errors: null,
+    });
+  } else return res.status(HTTP_STATUS_CODE.NOT_METHOD_ALLOWED).json(RESPONSE_NOT_METHOD_ALLOWED);
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,24 @@
-// import Image from "next/image";
-// import styles from "./page.module.css";
-
 import { Container } from "@/atom/Container";
 import { Heading } from "@/atom/Text";
 import Card from "@/components/Card";
 import { feeds } from "@/mocks";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+type Feed = typeof feeds;
+
+export const getServerSideProps = (async () => {
+  const res = await fetch("http://localhost:3000/api/feeds");
+  const feedList = (await res.json()).data;
+  return { props: { feedList } };
+}) satisfies GetServerSideProps<{ feedList?: Feed }>;
+
+export default function Home({ feedList }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div>
       <Heading>Slider Area</Heading>
       <Heading>Gates Area</Heading>
       <Container spacing={60}>
-        {feeds.map((feed) => (
+        {feedList?.map((feed: Feed[0]) => (
           <Card key={feed.feedNo} {...feed} />
         ))}
       </Container>
